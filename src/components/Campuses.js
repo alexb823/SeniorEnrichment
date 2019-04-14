ac23b6d7ac4bbe3510c3764a5ac73e2c19bcc1f1
Alex Bukhman <alexb823@gmail.com>
2019-04-14 11:17:00 -0400

    starting on Tier 2 frontend

--- src/components/Campuses.js
+++ src/components/Campuses.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { Container, Row, Col, Image } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Campuses = ({ campuses }) => {
   console.log(campuses);
@@ -12,14 +13,18 @@ const Campuses = ({ campuses }) => {
       <Container>
         <Row>
           {campuses.map(campus => (
-            <Col key={campus.id} xs={12} md={6} style={{padding: "0"}}>
+            <Col key={campus.id} xs={12} md={6} style={{ padding: '0' }}>
               <div
                 className="main-image m-1"
                 style={{ backgroundImage: `url(${campus.imageUrl})` }}
               >
-              <div className="fade-in-text align-middle">
-                <h1 className="display-4 text-white text-center">{campus.name}</h1>
-              </div>
+                <Link to={`/campuses/${campus.id}`}>
+                  <div className="fade-in-text align-middle">
+                    <h1 className="display-4 text-white text-center">
+                      {campus.name}
+                    </h1>
+                  </div>
+                </Link>
               </div>
             </Col>
           ))}
